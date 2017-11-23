f1ee5e407cbdd177e51bfb5fff394c00573cbdd9
Antonio Calapez <antonio@hackages.io>
2017-11-23 18:51:46 +0100

    Adding tests

--- react-bookstore/src/App.test.js
+++ react-bookstore/src/App.test.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, {mount} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
+import fs from 'fs';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('should be able to open the sidebar filters', () => {
+  const wrapper = mount(<App />);
+  wrapper.find('.filter-trigger').simulate('click');
+  expect(wrapper.find('.filter .filter-is-visible').length).toEqual(1);
+});
+
+it('should be able to close the sidebar filters', () => {
+  const wrapper = mount(<App />);
+  wrapper.find('.filter-trigger').simulate('click');
+  expect(wrapper.find('.filter .filter-is-visible').length).toEqual(1);
+  wrapper.find('.close').simulate('click');
+  expect(wrapper.find('.filter .filter-is-visible').length).toEqual(0);
+});
+
+it('should be able to change the selected category', () => {
+  const wrapper = mount(<App />);
+  const first = wrapper.find('li > a').at(1);
+  first.simulate('click');
+  expect(wrapper.find('li > a.selected').text()).toEqual(wrapper.find('li > a').at(1).text());
+});
+
+it('should have a lot of components', (done) => {
+  expect.assertions(1);
+  fs.readdir(__dirname + '/components', (_, data) => {
+    expect(data.length > 5).toEqual(true);
+    done();
+  });
 });
